aa4db4ff3d31245e8529fac9fedc21dc829ece46
ZEZE_COBRA <yeridsonruan995@gmail.com>
2024-08-17 18:00:57 -0300

    fixbug

---
+++ app/teste/layout.jsx
@@ -0,0 +1,7 @@
+export default function TesteLayout({ children }) {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
+}
--- app/teste/page.jsx
+++ app/teste/page.jsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import Card from './components/Card';
+import CardTeste from './components/cardTeste';
 
 export default function Teste() {
   const [todo, setTodo] = useState([]);
@@ -108,7 +108,7 @@ export default function Teste() {
         <div>
           {todo.map((todo) => {
             return (
-              <Card
+              <CardTeste
                 key={todo.id}
                 Id={todo.id}
                 Description={todo.text}
